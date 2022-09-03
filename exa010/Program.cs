e6c9aefe8a66fcf018c08dea677ce1b3fb10571c
annaloginova <a.vik00@mail.ru>
2022-09-03 14:34:13 +0300

    exa010

---
+++ exa010/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
