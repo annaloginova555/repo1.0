28f5b95c7016ca97eefee375fd902a199c44a762
annaloginova <a.vik00@mail.ru>
2022-09-05 16:04:52 +0300

    dz1_z4

---
+++ dz1_z4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+
+if (i % 2 == 1)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
